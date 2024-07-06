6304695e547f8c3b7a3fe544c4d264cdd1b7c465
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2024-07-06 21:04:07 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "MyEasyCart",
+    subtitle: "React E-commerce",
+    description:
+      "A React-based fully responsive e-commerce website, It uses the DummyJson API for fetching the real time data, Fromik and yup libraries are used for the form validation.",
+    image: ".././public/project1.png",
+    link: "https://myeasycart.netlify.app",
+  },
+  {
+    title: "Booking-Anytime",
+    subtitle: "React and Tailwind",
+    description:
+      "React-based travel booking website responsible for all screen sizes, Travel API is to display data according to user input. Search by place or hotel name, Unsplash API used to display images according to user input.",
+    image: ".././public/project2.png",
+    link: "https://booking-anytime.netlify.app",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "ToDos Manager app with timestamps so that you can easily track your tasks, add your tasks, edit or modify them, delete them after the it is done.",
+    image: ".././public/project3.png",
+    link: "https://mytodosmanager.netlify.app",
+  },
+  {
+    title: "React-routes55",
+    subtitle: "React Routes ",
+    description:
+      "React Router website, fully responsive for all screen sizes. It's a basic website showing the smooth workflow of react routes.",
+    image: ".././public/project4.png",
+    link: "https://react-routes55.netlify.app/",
+  },
+];
+
+export const skills = [
+  "HTML/CSS",
+  "Tailwind CSS",
+  "JavaScript",
+  "React",
+  "Rest APIs",
+  "Git/GitHub"
+];
