578a51db16a7798ceb1d76ea413e1723b9a8cc5d
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2024-07-06 21:22:55 +0530

    Images path in data.js updated

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "React E-commerce",
     description:
       "A React-based fully responsive e-commerce website, It uses the DummyJson API for fetching the real time data, Fromik and yup libraries are used for the form validation.",
-    image: ".././public/project1.png",
+    image: "/project1.png",
     link: "https://myeasycart.netlify.app",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "React and Tailwind",
     description:
       "React-based travel booking website responsible for all screen sizes, Travel API is to display data according to user input. Search by place or hotel name, Unsplash API used to display images according to user input.",
-    image: ".././public/project2.png",
+    image: "/project2.png",
     link: "https://booking-anytime.netlify.app",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "React Hooks",
     description:
       "ToDos Manager app with timestamps so that you can easily track your tasks, add your tasks, edit or modify them, delete them after the it is done.",
-    image: ".././public/project3.png",
+    image: "/project3.png",
     link: "https://mytodosmanager.netlify.app",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "React Routes ",
     description:
       "React Router website, fully responsive for all screen sizes. It's a basic website showing the smooth workflow of react routes.",
-    image: ".././public/project4.png",
+    image: "/project4.png",
     link: "https://react-routes55.netlify.app/",
   },
 ];
