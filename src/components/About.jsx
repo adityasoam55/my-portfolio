9c5af38de0e88783fbf4332f7c79e3160ae7034b
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2024-07-07 15:15:24 +0530

    Added animation in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,55 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-14 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <motion.div
+          className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
             Hi, I'm Aditya.
             <br className="lg:inline-block" />
             Front-End Developer.
           </h1>
           <p className="mb-8 leading-relaxed">
-          I am genuinely passionate about front-end development, and I'm eager to apply and expand my skills in a real-world setting and I'm quick to learn and adapt to new technologies.
+            I am genuinely passionate about front-end development, and I'm eager to apply and expand my skills in a real-world setting and I'm quick to learn and adapt to new technologies.
           </p>
           <div className="flex justify-center">
-            <a
+            <motion.a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
               Work With Me
-            </a>
-            <a
+            </motion.a>
+            <motion.a
               href="#projects"
-              className="ml-4 inline-flex text-gray-300 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              className="ml-4 inline-flex text-gray-300 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
               See My Past Work
-            </a>
+            </motion.a>
           </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        </motion.div>
+        <motion.div
+          className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1.2 }}
+        >
           <img
-            className=" w-full object-cover object-center rounded"
+            className="w-full object-cover object-center rounded"
             alt="hero"
             src="/myIMG.jpg"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
