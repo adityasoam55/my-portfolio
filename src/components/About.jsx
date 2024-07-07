a97de09c1130998dfdcb0dd4b335427e8472b602
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2024-07-07 14:52:52 +0530

    About Section main heading updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-14 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
             Hi, I'm Aditya.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="lg:inline-block" />
+            Front-End Developer.
           </h1>
           <p className="mb-8 leading-relaxed">
           I am genuinely passionate about front-end development, and I'm eager to apply and expand my skills in a real-world setting and I'm quick to learn and adapt to new technologies.
