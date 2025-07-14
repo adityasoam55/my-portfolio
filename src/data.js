68f6b283a391a1a920910939fc5ac99344a0bcd2
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-07-14 12:52:45 +0530

    Projects updated

--- src/data.js
+++ src/data.js
@@ -7,19 +7,29 @@ export const projects = [
     image: "/project4.png",
     link: "https://trycasuals.netlify.app/",
     skills: [
-      "React Js", "Tailwind CSS", "API Integration","User Aunthentication",
-    ]
+      "React Js",
+      "Tailwind CSS",
+      "API Integration",
+      "User Aunthentication",
+    ],
   },
   {
-    title: "Booking-Anytime",
-    subtitle: "React and Tailwind",
+    title: "PokemonVerse",
+    subtitle: "React - Pokémon App",
     description:
-      "React-based travel booking website responsive for all screen sizes, Uses TravelPayout API to display data according to user input. Search by place or hotel name, Unsplash API used to display images.",
-    image: "/project2.png",
-    link: "https://booking-anytime.netlify.app",
-    skills: [
-      "Tailwind CSS", "React Js", "API Integration",
-    ]
+      "A React-based responsive web app to explore Pokémon data with interactive cards, real-time search functionality, and a clean UI built with Tailwind CSS.",
+    image: "/project2.png", 
+    link: "https://pokemonverse.netlify.app",
+    skills: ["React Js", "Tailwind CSS", "REST API", "responsive-design"],
+  },
+  {
+    title: "Fanta World",
+    subtitle: "React - landing page",
+    description:
+      "A React-Framer motion based fully responsive  landing page,  smooth animations, a modern UI, and an intuitive user experience",
+    image: "/project1.png",
+    link: "https://fanta-fever.netlify.app",
+    skills: ["Tailwind CSS", "Framer-motion", "React Js", "responsive-design"],
   },
   {
     title: "Epic Todo App",
@@ -28,47 +38,33 @@ export const projects = [
       "ToDos Manager app with timestamps so that you can easily track your tasks, add your tasks, edit or modify them, delete them after task is completed.",
     image: "/project3.png",
     link: "https://mytodosmanager.netlify.app",
-    skills: [
-      "HTML", "Tailwind CSS", "JavaScript", "React Js",
-    ]
+    skills: ["HTML", "Tailwind CSS", "JavaScript", "React Js"],
   },
-  {
-    title: "Fanta World",
-    subtitle: "React - landing page",
-    description:
-      "A React-Framer motion based fully responsive  landing page,  smooth animations, a modern UI, and an intuitive user experience",
-    image: "/project1.png",
-    link: "https://level1-task2.netlify.app",
-    skills: [
-      "Tailwind CSS", "Framer-motion", "React Js", "responsive-design",
-    ]
-  }
 ];
-
 
 export const skills = [
   {
     title: "HTML/CSS",
-    icon: "IoLogoHtml5"
+    icon: "IoLogoHtml5",
   },
   {
     title: "Tailwind CSS",
-    icon: "RiTailwindCssFill"
+    icon: "RiTailwindCssFill",
   },
   {
     title: "JavaScript",
-    icon: "IoLogoJavascript"
+    icon: "IoLogoJavascript",
   },
   {
     title: "React",
-    icon: "IoLogoReact"
+    icon: "IoLogoReact",
   },
   {
     title: "Rest APIs",
-    icon: "AiTwotoneApi"
+    icon: "AiTwotoneApi",
   },
   {
     title: "Git/GitHub",
-    icon: "IoLogoGithub"
+    icon: "IoLogoGithub",
   },
 ];
