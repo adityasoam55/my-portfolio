6852b1e0013dfddc6ea0f5f4efd9d5de67bcc5b6
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-09-11 13:42:14 +0530

    Resume updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,10 @@ export default function About() {
 
   return (
     <section id="about">
-      <div ref={ref} className="container mx-auto flex px-10 py-14 md:flex-row flex-col items-center">
+      <div
+        ref={ref}
+        className="container mx-auto flex px-10 py-14 md:flex-row flex-col items-center"
+      >
         <motion.div
           className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
           initial={{ opacity: 0, y: 50 }}
@@ -23,11 +26,13 @@ export default function About() {
             Front-End Developer.
           </h1>
           <p className="mb-8 leading-relaxed">
-            I am genuinely passionate about front-end development, and I'm eager to apply and expand my skills in a real-world setting and I'm quick to learn and adapt to new technologies.
+            I am genuinely passionate about front-end development, and I'm eager
+            to apply and expand my skills in a real-world setting and I'm quick
+            to learn and adapt to new technologies.
           </p>
           <div className="flex justify-center">
             <motion.a
-              href="/AdityaSomResume.pdf"
+              href="/MyResume.pdf"
               download="AdityaSom-Resume.pdf"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
               whileHover={{ scale: 1.1 }}
@@ -48,7 +53,9 @@ export default function About() {
         <motion.div
           className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"
           initial={{ opacity: 0, scale: 0.8 }}
-          animate={inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }}
+          animate={
+            inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }
+          }
           transition={{ duration: 1.2 }}
         >
           <img
